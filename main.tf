4300612c6543e214636dd66dbec8fbcf5caaf0a1
Jeremiah Harlan <jeremiah.harlan@gmail.com>
2019-08-29 11:01:31 -0600

    task: remove cors from s3 bucket

--- main.tf
+++ main.tf
@@ -59,15 +59,15 @@ resource "aws_s3_bucket" "origin" {
   force_destroy = var.origin_force_destroy
   region        = data.aws_region.current.name
 
-  cors_rule {
-    allowed_headers = var.cors_allowed_headers
-    allowed_methods = var.cors_allowed_methods
-    allowed_origins = sort(
-      distinct(compact(concat(var.cors_allowed_origins, var.aliases))),
-    )
-    expose_headers  = var.cors_expose_headers
-    max_age_seconds = var.cors_max_age_seconds
-  }
+  # cors_rule {
+  #   allowed_headers = var.cors_allowed_headers
+  #   allowed_methods = var.cors_allowed_methods
+  #   allowed_origins = sort(
+  #     distinct(compact(concat(var.cors_allowed_origins, var.aliases))),
+  #   )
+  #   expose_headers  = var.cors_expose_headers
+  #   max_age_seconds = var.cors_max_age_seconds
+  # }
 }
 
 module "logs" {
